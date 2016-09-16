9573aa1205564bc327dbe229ce54d89cc67d3977
Lenn Hypolite <lenntriniman@hotmail.com>
2016-09-16 03:05:14 +0000

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # --route code
+        # path        controller action
+    get 'hello_world', to: 'static#hello_world' # --tells the Rails routing system that this route should be passed through the static controller's about action.
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
