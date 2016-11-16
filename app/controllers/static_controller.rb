984562bda4b855395833a84a4e67e0db0ae3e6a6
WilliamMena <williammena1991@gmail.com>
2016-11-16 10:05:27 -0500

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,7 @@
+class StaticController < ApplicationController
+
+  #have to make sure when creating controllers that the file name
+  #seperated with an '_' in between the name and the world controller
+  #in rails, I don't think you need to 'run' it inside of config
+
+end
