9b8599b3be6eff48506c62ba29c21d2d36fec7b0
github username <github email address>
2018-03-14 23:26:30 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+get 'hello_world' => 'static#hello_world'
+# The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
