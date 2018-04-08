c2e64b5f935767bdcece2171bdc43446192d19e8
Chris Harvey <chharvey@icloud.com>
2018-04-08 04:49:49 +0000

    Done.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,10 @@
 class StaticController < ApplicationController
 
   def hello_world
+<<<<<<< HEAD
     render "static/hello_world"
+=======
+>>>>>>> 07ff2257922ad8e9648c73aa8e53a6e94e0f9efc
   end
 
 end
