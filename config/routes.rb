7f76666c83e8a108d7f84824eca5e5f31435084c
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-31 01:56:20 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
 
   get '/hello_world', to: 'static#hello_world'
 
+
+    resources :posts, only: :show
+
+
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
