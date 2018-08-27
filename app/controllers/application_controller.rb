1481837c24a37839cb4b8c9123d46f8fba096d97
Frank McCallion <fkmccallion@gmail.com>
2018-08-27 19:14:37 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  get '/hello_world' do
-    erb: '/index'
-  end
-
 end
