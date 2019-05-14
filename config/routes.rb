efd1b427714090b13d0f97200daf44fd2d84de14
github username <github email address>
2019-05-14 12:50:41 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'about', to: 'static#about'
+  get 'hello_world', to: 'static#hello_world'
 
+    #The HTTP verb - in this case we're using the get HTTP verb.
+    #The path - 'hello_world' represents the path in the URL bar that the route will be mapped to
 end
