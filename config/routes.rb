aa34f716a5206484509687306ad84ac63f4935d8
Mohammed Noori <kmanoorib@gmail.com>
2019-05-17 23:44:19 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # example from previous lesson
+  # get 'about', to: 'static#about'
+  get 'hello_world', to: 'static#hello_world'
+  
 end
