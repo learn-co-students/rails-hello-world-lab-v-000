0c40cc30d1780cd9118f474497b3068c5227fb6a
Amee Covarrubias <ameecova@yahoo.com>
2019-11-05 01:39:05 +0000

    Done.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,8 @@
 class StaticController < ApplicationController
   def hello_world
   end
+<<<<<<< HEAD
 end
+=======
+end 
+>>>>>>> bf1b1a958d8bd6c562807bd7b310bff4530d716a
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+<<<<<<< HEAD
   get "hello_world", to: "static#hello_world"
+=======
+  
+resources :static
+>>>>>>> bf1b1a958d8bd6c562807bd7b310bff4530d716a
 end
